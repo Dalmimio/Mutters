1dcdfd9438a60bb26e22268f6bd50256759f8725
JoySalv <107821426+JoySalv@users.noreply.github.com>
2022-12-22 19:26:20 -0300

    subo header

--- src/components/HeaderApp.vue
+++ src/components/HeaderApp.vue
@@ -3,10 +3,383 @@
 </script>
 
 <template>
-
-        
-</template>
+    <!DOCTYPE html>
+ <html lang="en">
+   <head>
+     <meta charset="UTF-8" />
+     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+     
+     <link
+       rel="stylesheet"
+       href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0"
+     />
+     <link rel="stylesheet" href="styles.css" />
+   </head>
+   <body>
+     <aside class="sidebar">
+       <header class="sidebar-header">
+ 
+       </header>
+       <nav>
+         <button>
+           <span>
+             <i class="material-symbols-outlined"> home </i>
+             <span>Home</span>
+           </span>
+         </button>
+         <button>
+           <span>
+             <i class="material-symbols-outlined"> tag </i>
+             <span>About</span>
+           </span>
+         </button>
+         <button>
+           <span>
+             <i class="material-symbols-outlined">
+               notifications 
+             </i>
+             <span>Gallery</span>
+           </span>
+         </button>
+         <button>
+           <span>
+             <i class="material-symbols-outlined"> email </i>
+             <span>Contact</span>
+           </span>
+         </button>
+         <button>
+           <span>
+             <i class="material-symbols-outlined"> bookmark </i>
+             <span>Activities</span>
+           </span>
+         </button>
+         <button>
+           <span>
+             <i class="material-symbols-outlined"> person </i>
+             <span>Profile</span>
+           </span>
+         </button>
+         <button>
+           <span>
+             <i class="material-symbols-outlined"> expand_circle_down </i>
+             <span>More</span>
+           </span>
+         </button>
+      
+ 
+         <button class="user-button">
+           <span>
+             <img src="./icons/user.png" />
+             <span>
+               <span class="fullname"> Name </span>
+               <span class="username"> user </span>
+             </span>
+             <i class="material-symbols-outlined"> more_vert </i>
+           </span>
+         </button>
+       </nav>
+     </aside>
+     <div>
+       <nav class="mobile-bottom-nav">
+     <div class="mobile-bottom-nav__item mobile-bottom-nav__item--active">
+         <div class="mobile-bottom-nav__item-content">
+       <a class="btn botonera"> <img class="iconitos" src="./icons/home.png" alt="" srcset=""> </a>
+             
+         </div>		
+     </div>
+     <div class="mobile-bottom-nav__item">		
+         <div class="mobile-bottom-nav__item-content">
+       <a class="btn botonera"><img class="iconitos" src="./icons/paint.png" alt="" srcset=""> </a>
+         
+         </div>
+     </div>
+     <div class="mobile-bottom-nav__item">
+         <div class="mobile-bottom-nav__item-content">
+       <a class="btn botonera"> <img class="iconitos" src="./icons/profile.png" alt="" srcset=""> </a>
+             
+         </div>		
+     </div>
+     
+     <div class="mobile-bottom-nav__item">
+         <div class="mobile-bottom-nav__item-content">
+           <a class="botonera btn "><img class="iconitos" src="./icons/more.png" alt="" srcset=""> </a>
+             
+         </div>		
+     </div>
+ </nav>
+     </div>
+   </body>
+ </html>
+ </template>
 
 <style scoped>
+* {
+  box-sizing: border-box;
+}
+
+body {
+  margin: 0;
+  background: #000000;
+  color: #ffffff;
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+    Ubuntu, "Helvetica Neue", sans-serif;
+}
+
+.iconitos{
+  display:none
+}
+
+.material-symbols-outlined {
+
+  font-size: 22px;
+  color:#f5f5f5;
+  background-color:#CADEC8;
+  border-radius: 50%;
+  padding: 7px 7px;
+  
+  
+  
+
+}
+
+.sidebar {
+  position: fixed;
+  top: 0;
+  left: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  width: 250px;
+  height: 100%;
+  padding: 0px 10px 30px 10px;
+  border-right: 1px solid #CADEC8;
+  background-color: #f5f5f5;
+  font-family: 'Quicksand';
+
+}
+
+
+.sidebar-header {
+  width: 100%;
+  background-image: url(./icons/logo.png);
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
+  display: flex;
+  justify-content: center;
+  padding-top: 18vh;
+  background-size:32vh;
+
+}
+
+
+
+
+
+ @media screen and (max-width: 577px) and (min-width: 425px) {
+  .sidebar-header {
+  width: 100%;
+  background-image: url(./icons/logopeque.png);
+  background-repeat: no-repeat;
+  background-position: center;
+  background-repeat: no-repeat;
+  display: flex;
+  justify-content: center;
+  background-size:10vh
+
+}
+
+nav{ 
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding-right: 2vh;
+
+ 
+
+}
+  
+  
+ }
+
+
+.sidebar button {
+  height: 60px;
+  background: transparent;
+  border: 0;
+  padding: 0;
+  font-family: inherit;
+  color: inherit;
+  cursor: pointer;
+  text-align: left;
+}
+
+.sidebar button > span {
+  display: inline-flex;
+  align-items: center;
+  gap: 12px;
+  height: 48px;
+  padding: 0 16px 0 12px;
+  border-radius: 24px;
+  line-height: 1;
+}
+
+.sidebar button:hover > span {
+  background: rgba(255, 255, 255, 0.08);
+}
+
+.sidebar button i {
+  position: relative;
+  font-size: 28px;
+  transition: 0.2s;
+}
+
+.sidebar button img {
+  width: 40px;
+ 
+  
+}
+
+.sidebar button i > span {
+  display: grid;
+  place-items: center;
+  padding: 2px 5px;
+  border-radius: 10px;
+  position: absolute;
+  top: -5px;
+  left: 50%;
+  translate: -50% 0;
+  background: #1d9bf0;
+  color: #f9f9f9;
+  font-size: 10px;
+  font-family: BlinkMacSystemFont;
+  font-style: normal;
+}
+
+.sidebar button span {
+  font-size: 17px;
+}
+
+.sidebar > nav {
+  flex: 1 1 auto;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+}
+
+.sidebar > nav button:last-child {
+  margin-top: auto;
+}
+
+
+button.user-button .fullname {
+  font-size: 15px;
+}
+
+button.user-button .username {
+  color: rgba(255, 255, 255, 0.4);
+  font-size: 12px;
+}
+
+
+button.user-button span {
+  width: 100%;
+}
+
+button.user-button span > span {
+  display: grid;
+  gap: 4px;
+  width: 100%;
+}
+
+button.user-button span > i {
+  font-size: 20px;
+  margin-left: auto;
+}
+
+
+
+
+@media (width < 580px ) and (width > 425px){
+  .sidebar {
+    width: 72px;
+  }
+
+  
+
+  .botonera {
+    
+    display:flex;
+    flex-direction: row;
+    justify-content: space-between;
+    width: 100%;
+  }
+ 
+
+  .sidebar button > span {
+    width: 50px;
+  }
+
+  .sidebar button > span > span {
+    opacity: 0;
+    display:none;
+  }
+
+
+  button.user-button span > i {
+    display: none;
+  }
+}
+
+
+
+@media screen and (max-width: 425px){
+
+.sidebar{
+  display: none;
+}
+.mobile-bottom-nav {
+	position: fixed;
+	bottom: 0;
+	left: 0;
+	right: 0;
+	z-index: 1000;
+  will-change: transform;
+	transform: translateZ(0);
+
+	display: flex;
+  flex-direction: row;
+  justify-content:space-around;
+  align-items: center;
+ 
+  
+
+
+	height: 60px;
+  border-top: 2px solid #CADEC8;
+
+	background-color: #F5F5F5;
+
+
+}
+
+.iconitos{
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+  width: 10vh;
+  padding-bottom: 2vh;
+ 
+  
+  
+
+
+}
+  
+
+}
 
 </style>
