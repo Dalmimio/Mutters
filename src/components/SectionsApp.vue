221d86ea28f811c65a4fe875d949c4142cd659e8
JoySalv <107821426+JoySalv@users.noreply.github.com>
2022-12-24 15:09:30 -0300

    componente secciones con sus imagenes

---
+++ src/components/SectionsApp.vue
@@ -0,0 +1,112 @@
+<script setup>
+import SideBarApp from './SideBarApp.vue';
+</script>
+
+<template>
+
+
+  <div class="contenedor">
+    <SideBarApp />
+    <div class="verde">
+      <div>
+        <h1>Do you want to relax or distract yourself for a moment? Try one of our sections.</h1>
+      </div>
+
+
+
+      <div class="sections">
+        <div class="section">
+
+          <img src="./icons/breath.png" alt="" srcset="">
+          <button></button>
+        </div>
+        <div class="section">
+
+          <img src="./icons/pinta.png" alt="" srcset="">
+          <button></button>
+        </div>
+        <div class="section">
+
+          <img src="./icons/share.png" alt="" srcset="">
+          <button></button>
+        </div>
+
+      </div>
+      <div>
+        <button>Let's meet Muzzu!</button>
+
+      </div>
+
+    </div>
+
+
+  </div>
+
+
+
+
+
+</template>
+
+<style scoped>
+.sections {
+
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+}
+
+.section {
+  border-radius: 4px;
+  margin: 1vh;
+}
+
+img {
+  width: 100%;
+  height: 100%;
+}
+
+.verde {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  text-align: center;
+  min-height: 100vh;
+  justify-content: space-around;
+}
+
+button {
+  background-color: #7abd87;
+  height: 10vh;
+  border: none;
+  border-radius: 10px;
+  font-size: 4vh;
+  color: white;
+}
+
+
+
+.contenedor {
+
+  display: flex;
+  background-color: #cadec8;
+  font-family: 'Quicksand';
+}
+
+
+
+
+
+@media screen and (max-width: 425px) {
+
+  .verde {
+    align-items: center;
+    width: 100%;
+    justify-content: space-around;
+    min-height: calc(100vh - 60px);
+  }
+
+}
+</style>
