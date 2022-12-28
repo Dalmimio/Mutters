598650e096340b98fce30d5cd52e75c6c7c73ba7
JoySalv <107821426+JoySalv@users.noreply.github.com>
2022-12-28 16:16:53 -0300

    cambios joha

--- src/components/GalleryApp.vue
+++ src/components/GalleryApp.vue
@@ -15,41 +15,54 @@ onMounted(() => {
 
 </script>
 
-<template style="background-color: #f5f5f5">
+<template>
 
 
 
+  <div class="contenedor">
+    <HeaderApp />
+    <div class="verde">
+      <div class="espacio-nav">
+        </div>
+
+        <div>
 
 
+          <div>
+
+    <div>
 
 
+        <div class="header">
 
-
-    <div
-        style="background-color: #f5f5f5; height: 100vh; display: flex; font-family: 'Quicksand'; flex-direction: column;">
-
-
-        <div class="title w-100 d-flex flex-column align-items-center justify-content-center text-center">
             <h1>Exhibitions</h1>
-            <h5>Browse and comment on shared paintings or</h5>
-            <button class="btn btn-primary mt-2">create your own</button>
+            <h2>Browse and comment on shared paintings or</h2>
+            <RouterLink to="./newpaint"><button class="btn btn-primary mt-2">Create your own <img class="lapicito" src="../assets/img/paint_icon.png" alt="" srcset="">  </button></RouterLink>
+            
           </div>
 
-          <div class="contenedorCards d-flex gap-5 flex-wrap justify-content-around mt-4">
+          <div class="cards">
 
-            <div class="card d-flex flex-column cardi align-items-center text-center" v-for="pintura in pinturas">
-              <span style="font-weight: bold;">Author: </span>{{ pintura.autor }}
+            <div class="card" v-for="pintura in pinturas">
+
+              <div class="autor">
+                <div class="author">
+                  <span>Author: </span> {{ pintura.autor }}
+                </div>
+              
+             
               <img class="w-100" :src="pintura.urlImage">
+            </div>
 
 
+              <h2>{{ pintura.name }}</h2>
 
-              <h4>{{ pintura.name }}</h4>
+              <h4> {{ pintura.description }}</h4>
 
-              <p> {{ pintura.description }}</p>
 
               <NewComment />
 
-                <Comments class="w-100" />
+           
 
             </div>
 
@@ -63,6 +76,9 @@ onMounted(() => {
 
     </div>
   </div>
+    </div>
+   
+
 
 
 
@@ -75,6 +91,27 @@ onMounted(() => {
   width: 25vw;
 }
 
+
+
+.header{
+  display: flex;
+  flex-direction: column;
+  justify-content:center;
+  align-items: center;
+  align-self: center;
+  gap: 2vh;
+
+
+}
+
+.lapicito{
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 10%;
+  height:   10%;
+}
+
 .verde {
   display: flex;
   flex-direction: column;
@@ -84,14 +121,59 @@ onMounted(() => {
   min-height: 100vh;
   justify-content: space-around;
   padding: 2vh;
+  background-color:#EAEEE5 ;
+  
 }
+
+.card{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 2vh;
+  padding: 2vh;
+  background-color:#f5f5f5;
+  margin: 4vh;
+}
+
+ .cards{
+  margin: 2vh;
+
+ }
+
+ .autor{
+  flex-direction: row;
+  align-items: flex-start;
+  padding-bottom: 1vh;
+ }
+
+ .author{
+  font-size: larger;
+  display: flex;
+  flex-direction: row;
+  gap: 1vh;
+  padding-bottom: 1vh;
+  background-color: #7abd87;
+  padding: 1vh;
+  border-top-left-radius: 5px;
+  border-top-right-radius: 5px;
+  margin-bottom: 1vh;
+  color:white;
+  font-family: 'Quicksand';
+ }
+
+
+
 
 
 button {
   background-color: #7abd87;
-  height: 5vh;
+ display: flex;
+ align-items: center;
+ font-size: 3vh;
   border: none;
   border-radius: 10px;
+  width: 100%;
+  justify-content: center;
 }
 
 
@@ -109,6 +191,7 @@ button {
 
 
 
+
 @media screen and (max-width: 425px) {
 
   .verde {
@@ -117,11 +200,6 @@ button {
     width: 100%;
     justify-content: space-around;
     min-height: calc(100vh - 60px);
-
-
-
-
-
   }
 
 }
